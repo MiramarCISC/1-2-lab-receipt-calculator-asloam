0b6690d6033885a81f9e4cf507fb54dd285ad482
asloam <155298294+asloam@users.noreply.github.com>
2026-06-22 16:37:13 +0000

    Adjusted readability

--- src/receipt.cpp
+++ src/receipt.cpp
@@ -2,19 +2,19 @@
 
 // TODO: Return the sum of all three item prices.
 double calculateSubtotal(double item1, double item2, double item3) {
-    return (item1+item2+item3);
+    return item1 + item2 + item3;
 }
 
 // TODO: Return the sales tax for the subtotal using TAX_RATE.
 double calculateTax(double subtotal) {
-    return subtotal*TAX_RATE;
+    return subtotal * TAX_RATE;
 }
 
 // TODO: Return the tip amount.
 // Example: if subtotal is 20.00 and tipPercent is 15,
 // the tip should be 3.00.
 double calculateTip(double subtotal, double tipPercent) {
-    return subtotal*(tipPercent/100);
+    return subtotal * (tipPercent / 100);
 }
 
 // TODO: Return the final total.
