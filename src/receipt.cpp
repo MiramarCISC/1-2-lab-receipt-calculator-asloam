be8bee76a1ff0b14b0bee632ff0a3663c4acb7da
NicC28 <ncheng003@student.sdccd.edu>
2026-06-29 01:52:41 +0000

    I revised your code by cleaning up the comments to look more like real code not an assignment and adjusted some calculation orders to make it more readable.

--- src/receipt.cpp
+++ src/receipt.cpp
@@ -1,23 +1,27 @@
 #include "receipt.hpp"
 
-// TODO: Return the sum of all three item prices.
-double calculateSubtotal(double item1, double item2, double item3) {
-    return item1 + item2 + item3;
+
+double calculateSubtotal(double item1, double item2, double item3) 
+{
+    return item1 + item2 + item3; //Returns the sum of all three item prices.
 }
 
-// TODO: Return the sales tax for the subtotal using TAX_RATE.
-double calculateTax(double subtotal) {
-    return subtotal * TAX_RATE;
+
+double calculateTax(double subtotal) 
+{
+    return subtotal * TAX_RATE; //Returns the sales tax for the subtotal using TAX_RATE.
 }
 
-// TODO: Return the tip amount.
+
 // Example: if subtotal is 20.00 and tipPercent is 15,
 // the tip should be 3.00.
-double calculateTip(double subtotal, double tipPercent) {
-    return (subtotal * tipPercent)/ 100.0;
+double calculateTip(double subtotal, double tipPercent) 
+{
+    return subtotal * (tipPercent/100.0); //Returns the tip amount.
 }
 
-// TODO: Return the final total.
-double calculateTotal(double subtotal, double tax, double tip) {
-    return subtotal + tax + tip;
+
+double calculateTotal(double subtotal, double tax, double tip) 
+{
+    return subtotal + tax + tip; //Returns the final total.
 }
